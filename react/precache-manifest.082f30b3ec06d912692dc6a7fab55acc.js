116bafdc29e0091bbce3c624b585fb6e71958a5c
Nunmuthu <nunmuthu@gmail.com>
2019-12-10 11:35:45 +0530

    Added react project

---
+++ react/precache-manifest.082f30b3ec06d912692dc6a7fab55acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66340d41af4e1121611669d086b612c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0703636b5366466b9c7",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d66f592f9e96aaafc08d",
+    "url": "/static/css/main.9b5af2a1.chunk.css"
+  },
+  {
+    "revision": "d0703636b5366466b9c7",
+    "url": "/static/js/2.c9a9b4a2.chunk.js"
+  },
+  {
+    "revision": "d66f592f9e96aaafc08d",
+    "url": "/static/js/main.8efc9f5c.chunk.js"
+  },
+  {
+    "revision": "5fa28fa4c207d199b759",
+    "url": "/static/js/runtime-main.6a7d632f.js"
+  }
+]);
